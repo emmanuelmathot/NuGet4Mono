c6a6050f029164b32cff18d2a164ef3b5d618c2f
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-03-03 10:38:11 +0100

    assembly description

--- NuGet4Mono.Extensions/Properties/AssemblyInfo.cs
+++ NuGet4Mono.Extensions/Properties/AssemblyInfo.cs
@@ -6,20 +6,18 @@ using System.Runtime.CompilerServices;
 using NuGet4Mono.Extensions;
 
 [assembly: AssemblyTitle("NuGet4Mono.Extensions")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Terradue")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Terradue")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("Extensions for using nuget with projects developed with Mono")]
 [assembly: AssemblyAuthors("Emmanuel Mathot")]
+[assembly: AssemblyIconUrl("")]
+[assembly: AssemblyProjectUrl("https://github.com/emmanuelmathot/NuGet4Mono")]
+[assembly: AssemblyLicenseUrl("https://github.com/emmanuelmathot/NuGet4Mono/License")]
+[assembly: AssemblyVersion("0.2.0.*")]
+[assembly: AssemblyInformationalVersion("0.2.0")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
