6bdac1691ec1f91d30a50216cc122e4c4bdb6518
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-03-03 10:49:48 +0100

    up version

--- NuGet4Mono.Extensions/Properties/AssemblyInfo.cs
+++ NuGet4Mono.Extensions/Properties/AssemblyInfo.cs
@@ -11,8 +11,8 @@ using NuGet4Mono.Extensions;
 [assembly: AssemblyIconUrl("")]
 [assembly: AssemblyProjectUrl("https://github.com/emmanuelmathot/NuGet4Mono")]
 [assembly: AssemblyLicenseUrl("https://github.com/emmanuelmathot/NuGet4Mono/License")]
-[assembly: AssemblyVersion("0.2.1.*")]
-[assembly: AssemblyInformationalVersion("0.2.1")]
+[assembly: AssemblyVersion("0.3.0.*")]
+[assembly: AssemblyInformationalVersion("0.3.0")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
