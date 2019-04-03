a381cd93c3e23736379951dc33b7cc1ba147509e
Emmanuel Mathot <emmanuel.mathot@terradue.com>
2019-04-03 08:53:29 +0200

    ready to pack 0.7.0
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@terradue.com>

--- NuGet4Mono/Properties/AssemblyInfo.cs
+++ NuGet4Mono/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using NuGet4Mono.Extensions;
 [assembly: AssemblyIconUrl("")]
 [assembly: AssemblyProjectUrl("https://github.com/emmanuelmathot/NuGet4Mono")]
 [assembly: AssemblyLicenseUrl("https://github.com/emmanuelmathot/NuGet4Mono/License")]
-[assembly: AssemblyVersion("0.6.0.*")]
-[assembly: AssemblyInformationalVersion("0.6.0")]
+[assembly: AssemblyVersion("0.7.0.*")]
+[assembly: AssemblyInformationalVersion("0.7.0")]
 
 
