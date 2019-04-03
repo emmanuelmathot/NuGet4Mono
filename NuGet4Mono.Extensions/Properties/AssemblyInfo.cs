9e40f9457206ac6201c17e40b292ffbf7f561a64
Emmanuel Mathot <emmanuel.mathot@terradue.com>
2019-04-03 10:08:20 +0200

    bootstrap
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@terradue.com>

--- NuGet4Mono.Extensions/Properties/AssemblyInfo.cs
+++ NuGet4Mono.Extensions/Properties/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿using System.Reflection;
-using NuGet4Mono.Extensions;
 
 [assembly: AssemblyTitle("NuGet4Mono.Extensions")]
 [assembly: AssemblyDescription("Extensions for using nuget with projects developed with Mono")]
-[assembly: AssemblyAuthors("Emmanuel Mathot")]
-[assembly: AssemblyIconUrl("")]
-[assembly: AssemblyProjectUrl("https://github.com/emmanuelmathot/NuGet4Mono")]
-[assembly: AssemblyLicenseUrl("https://github.com/emmanuelmathot/NuGet4Mono/License")]
+[assembly: NuGet4Mono.Extensions.AssemblyAuthors("Emmanuel Mathot")]
+[assembly: NuGet4Mono.Extensions.AssemblyIconUrl("")]
+[assembly: NuGet4Mono.Extensions.AssemblyProjectUrl("https://github.com/emmanuelmathot/NuGet4Mono")]
+[assembly: NuGet4Mono.Extensions.AssemblyLicenseUrl("https://github.com/emmanuelmathot/NuGet4Mono/License")]
 [assembly: AssemblyVersion("0.7.0.*")]
 [assembly: AssemblyInformationalVersion("0.7.0")]
 
